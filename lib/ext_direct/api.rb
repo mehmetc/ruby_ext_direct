42c57c62138d6bbc7092b48d52e12a3061799fe3
Mehmet Celik <mehmet@celik.be>
2012-03-12 23:18:11 +0100

    bug fixes, route can be set now

--- lib/ext_direct/api.rb
+++ lib/ext_direct/api.rb
@@ -4,8 +4,9 @@ module ExtDirect
 # Simply expose a class 
 # @author Mehmet Celik
   class Api
-    attr_reader :exposed_api
-
+    @exposed_api_raw = nil
+    @router_url = '/router'
+    
 # Expose a class
 #
 # @author Mehmet Celik
@@ -13,7 +14,7 @@ module ExtDirect
 # @param [Hash] instructions to how to expose the class. For now just the ':only' key is accepted.
 # @return [Hash] returns a list of exposed classes
     def self.expose(class_to_expose, options = {})
-      @exposed_api = {} if @exposed_api.nil?
+      @exposed_api_raw = {} if @exposed_api_raw.nil?
       
       methods = []
       
@@ -21,16 +22,21 @@ module ExtDirect
         raw_methods = options[:only] || []
       else
         raw_methods = class_to_expose.instance_methods(false) - (options[:except] || [])
+        raw_methods += class_to_expose.methods(false) - (options[:except] || [])        
       end
       
       raw_methods.uniq!
       raw_methods.each do |m|
         name = m
-        len = class_to_expose.instance_method(m).parameters.size
-        methods << {:name => name, :len => len}
+#        len = class_to_expose.method(m).parameters.size
+        parameters = []
+        class_to_expose.method(m).parameters.each do |p|
+          parameters << {:name => p[1].to_s, :optional => p[0] == :opt}
+        end
+        methods << {:name => name, :parameters => parameters}
       end
             
-      @exposed_api.store(class_to_expose.name, methods)
+      @exposed_api_raw.store(class_to_expose.name, methods)
     end
     
 # Expose all classes in a directory(conviniance method)
@@ -38,7 +44,7 @@ module ExtDirect
 # @author Mehmet Celik
 # @param [String] Directory where all classes are stored
     def self.expose_all(class_dir)
-      @exposed_api = {}
+      @exposed_api_raw = {}
      
       Dir.glob("#{class_dir}/**/*.rb").each do |r|
         rr = r.split("#{class_dir}/")[1].gsub('.rb','')     
@@ -64,9 +70,37 @@ module ExtDirect
 # @author Mehmet Celik
 # @return [Hash] exposed classes
     def self.to_raw
-      api = {:url => '/router', 
+      
+      api = {:url => @router_url || '/router', 
              :type => 'remoting',
-             :actions => @exposed_api}      
+             :actions => self.exposed_api}
+    end
+# Set the router url
+
+# @author Mehmet Celik   
+# @param [String] router url defaults to '/router'
+    def self.router_url=(url = '/router')
+      @router_url = url
+    end
+    
+    def self.router_url
+      @router_url
+    end
+    
+    def self.exposed_api(show_parameters = false)
+      result = @exposed_api_raw
+      unless show_parameters
+        result = {}
+        @exposed_api_raw.each do |k,v|
+          methods = []
+          v.each do |method|
+            methods << {:name => method[:name], :len => method[:parameters].size}
+          end
+          result.store(k,methods)
+        end
+      end
+      
+      result
     end
   end
 end
